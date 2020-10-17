0e35fe74d832a638cc6829b86863169b54abb673
Bayma Bruno <baymabruno@gmail.com>
2020-10-17 16:03:55 -0300

    55. Componente Botão #02

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,12 +12,36 @@ const style = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#888',
   },
+  oparetionButton: {
+    color: '#fff',
+    backgroundColor: '#fa8231',
+  },
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3,
+  },
 });
 
 export default (props) => {
+  const stylesButton = [style.button];
+
+  if (props.double) {
+    stylesButton.push(style.buttonDouble);
+  }
+
+  if (props.triple) {
+    stylesButton.push(style.buttonTriple);
+  }
+
+  if (props.operation) {
+    stylesButton.push(style.oparetionButton);
+  }
+
   return (
-    <TouchableHighlight onPress={props.onClick}>
-      <Text style={style.button}>{props.label}</Text>
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
+      <Text style={stylesButton}>{props.label}</Text>
     </TouchableHighlight>
   );
 };
